68d1a5698235302301e471a204e2f4811c9d9a97
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-09-15 17:42:34 +0700

    fix task4.sh

--- solution/task4.sh
+++ solution/task4.sh
@@ -1,1 +1,1 @@
-cat data/blok.txt | sed "s/Живи/Не спи/g; s/Умрёшь/Уснёшь/g" | > data/blok_kids_edition.txt
+cat data/blok.txt | sed "s/Живи/Не спи/g; s/Умрёшь/Уснёшь/g" > data/blok_kids_edition.txt
